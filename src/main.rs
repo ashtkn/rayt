182144dc15084503225bd1a7bc16c308102c79ec
Asahi Takenouchi <asahi.taken@gmail.com>
2023-12-18 23:54:00 +0900

    feat: implement hit_sphere

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,22 @@ use crate::rayt::*;
 const IMAGE_WIDTH: u32 = 200;
 const IMAGE_HEIGHT: u32 = 100;
 
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * ray.direction.dot(oc);
+    let c = oc.dot(oc) - radius.powi(2);
+    let d = b * b - 4.0 * a * c;
+    d > 0.0
+}
+
 fn color(ray: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let d = ray.direction.normalize();
     let t = 0.5 * (d.y() + 1.0);
-    Color::new(0.6, 0.7, 1.0).lerp(Color::one(), t)
+    Color::new(0.5, 0.7, 1.0).lerp(Color::one(), t)
 }
 
 fn main() {
