a27f48ae75a32bb944644ebe4d89da829a308b0c
Asahi Takenouchi <asahi.taken@gmail.com>
2023-12-19 12:22:30 +0900

    feat: implement hit_sphere with normal vector

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,25 @@ use crate::rayt::*;
 const IMAGE_WIDTH: u32 = 200;
 const IMAGE_HEIGHT: u32 = 100;
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(ray.direction);
     let b = 2.0 * ray.direction.dot(oc);
     let c = oc.dot(oc) - radius.powi(2);
     let d = b * b - 4.0 * a * c;
-    d > 0.0
+    if d < 0.0 {
+        -1.0
+    } else {
+        return (-b - d.sqrt()) / (2.0 * a);
+    }
 }
 
 fn color(ray: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &ray) {
-        return Color::new(1.0, 0.0, 0.0);
+    let c = Point3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(c, 0.5, &ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - c).normalize();
+        return 0.5 * (n + Vec3::one());
     }
     let d = ray.direction.normalize();
     let t = 0.5 * (d.y() + 1.0);
